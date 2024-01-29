ae6a791fc2b2f7465931ea7fc4c21bb1fa5e28f2
PaulTitto <paulustito555@gmail.com>
2024-01-30 05:57:37 +0700

    Transaction Midtrans endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,11 @@
-RUN go build -o main .
-
 FROM golang:latest
 
 WORKDIR /app/campaignweb
 
-COPY --from=0 /app/main .
+COPY . .
 
+RUN go build -o main .
 
 EXPOSE 8080
-
-
-ENV DB_HOST=localhost
-ENV DB_PORT=8080
-ENV DB_USER=myuser
-ENV DB_PASSWORD=mypassword
-ENV DB_NAME=mydatabase
-
 
 CMD ["./main"]
--- handler/transaction.go
+++ handler/transaction.go
@@ -83,3 +83,25 @@ func (h *transactionHandler) CreateTransactions(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 
 }
+
+func (h *transactionHandler) GetNotification(c *gin.Context) {
+	var input transaction.TransactionNotificationInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		response := helper.APIResponse("Failed to process Transaction", http.StatusUnprocessableEntity, "error", err)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	err = h.service.ProcessPayment(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to process Transaction", http.StatusUnprocessableEntity, "error", err)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	c.JSON(http.StatusOK, input)
+
+}
